442ed9070b964b3c18c392e36f176e17c9d4332c
João Bragança <joaobraganca555@gmail.com>
2024-02-13 17:39:20 +0000

    Refactor iframe styling in Form component

--- src/views/Form.jsx
+++ src/views/Form.jsx
@@ -1,17 +1,27 @@
 const Form = () => {
   return (
-    <iframe
-      src="https://docs.google.com/forms/d/e/1FAIpQLSecdQw-XpDMdONQiVK2qWEH1AloGOnvjWdPQKCM8uBeBGbfNA/viewform?embedded=true"
-      width="640"
-      height="2349"
+    <div
       style={{
-        border: 0,
-        marginHeight: 0,
-        marginWidth: 0
+        overflow: 'hidden',
+        paddingTop: '100%', // Maintain the aspect ratio padding
+        position: 'relative', // Ensure the container has a relative positioning context
+        width: '100%' // Ensure the container takes the full width of the parent
       }}
     >
-      A carregar…
-    </iframe>
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSecdQw-XpDMdONQiVK2qWEH1AloGOnvjWdPQKCM8uBeBGbfNA/viewform?embedded=true"
+        style={{
+          position: 'absolute',
+          top: '20px', // Pushes the iframe down by 100px
+          left: 0,
+          width: '100%',
+          height: 'calc(100% - 100px)', // Adjust the height to account for the top margin, preventing extra scroll space
+          border: 0
+        }}
+      >
+        A carregar…
+      </iframe>
+    </div>
   );
 };
 
