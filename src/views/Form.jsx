15a0fb31985bd259705ce3eef084fa42f59a0f76
João Bragança <joaobraganca555@gmail.com>
2024-02-13 17:48:55 +0000

    Refactor iframe styling in Form component

--- src/views/Form.jsx
+++ src/views/Form.jsx
@@ -3,20 +3,16 @@ const Form = () => {
     <div
       style={{
         overflow: 'hidden',
-        paddingTop: '100%', // Maintain the aspect ratio padding
-        position: 'relative', // Ensure the container has a relative positioning context
         width: '100%' // Ensure the container takes the full width of the parent
       }}
     >
       <iframe
         src="https://docs.google.com/forms/d/e/1FAIpQLSecdQw-XpDMdONQiVK2qWEH1AloGOnvjWdPQKCM8uBeBGbfNA/viewform?embedded=true"
         style={{
-          position: 'absolute',
-          top: '20px', // Pushes the iframe down by 100px
-          left: 0,
           width: '100%',
-          height: 'calc(100% - 100px)', // Adjust the height to account for the top margin, preventing extra scroll space
-          border: 0
+          height: 'calc(100vh - 100px)', // Use viewport height minus the top margin to determine the height
+          border: 0,
+          marginTop: '10px' // Use margin-top to push the iframe down without affecting its height
         }}
       >
         A carregar…
